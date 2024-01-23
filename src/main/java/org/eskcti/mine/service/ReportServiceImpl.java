63188b4f7ccbd36571eb42958b81a59ad465a3ee
eskokado <eskokado@gmail.com>
2024-01-23 06:46:38 -0300

    feat: impl ReportServiceImpl

--- src/main/java/org/eskcti/mine/service/ReportServiceImpl.java
+++ src/main/java/org/eskcti/mine/service/ReportServiceImpl.java
@@ -1,20 +1,30 @@
 package org.eskcti.mine.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eskcti.mine.client.ReportRestClient;
 import org.eskcti.mine.dto.OpportunityDTO;
+import org.eskcti.mine.utils.CSVHelper;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
 
 @ApplicationScoped
 public class ReportServiceImpl implements ReportService{
+    @Inject
+    @RestClient
+    ReportRestClient reportRestClient;
+
     @Override
     public List<OpportunityDTO> getOpportunityData() {
-        return null;
+        return reportRestClient.requestOpportunitiesData();
     }
 
     @Override
     public ByteArrayInputStream generateCSVOpportunityReport() {
-        return null;
+        List<OpportunityDTO> opportunities = reportRestClient.requestOpportunitiesData();
+
+        return CSVHelper.OpportunitiesToCSV(opportunities);
     }
 }
